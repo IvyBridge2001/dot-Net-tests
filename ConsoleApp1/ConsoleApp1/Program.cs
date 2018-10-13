0b087acddf4a5becbcf0e9b6586c7c06e07bdc3d
IvyBridge2001 <ivybridge95@outlook.com>
2018-10-12 23:36:57 -0300

    First commit
    App1 ok, App2 not working yet

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+    internal class Tutorial
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader sr1 = File.OpenText(@"D:\Example.txt"))
+            using (StreamWriter sr2 = File.AppendText(@"D:\Example.log"))
+            {
+                String s = "";
+
+                while ((s = sr1.ReadLine()) != null)
+                {
+                    string[] temp = s.Split(", ");
+                    sr2.WriteLine(temp[0]);
+                    
+                }
+                sr2.Close();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
